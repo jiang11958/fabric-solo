f47f144e5812b0497308efae2756117a1369b7c8
jiangzhiwen <0027013056@iwhalecloud.com>
2020-07-04 15:51:52 +0800

    fix

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ function installPkg(){
 	fi
 
 	##### install docker-compose
-	if ! [ -x "$(command -v docker)" ]; then
+	if ! [ -x "$(command -v docker-compose)" ]; then
 		echo "install docker-compose..."
 		sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 		sudo chmod +x /usr/local/bin/docker-compose
